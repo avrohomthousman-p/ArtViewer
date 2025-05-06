dac4e0362fd3d56895a3df4b54cc861d9050cb9d
avrohomthousman-p <avrohomthousman@gmail.com>
2025-05-06 14:56:27 -0400

    feat: setup basic system for querying API

--- ArtViewer/Activities/DisplayActivity.cs
+++ ArtViewer/Activities/DisplayActivity.cs
@@ -1,4 +1,5 @@
 ﻿using AndroidX.RecyclerView.Widget;
+using ArtViewer.Network.Deviantart;
 
 namespace ArtViewer.Activities;
 
@@ -19,33 +20,8 @@ public class DisplayActivity : Activity
     private async Task SetupRecyclerView()
     {
         //Use sample images until the API queries are set up
-        List<string> imageUrls = new List<string>();
-        imageUrls.Add("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/cc64ef64-3d04-4b32-b650-ec8aac8c6d5b/dji89sc-c" +
-            "b686366-ac3b-41cb-a6cb-7c1732510e14.png/v1/fill/w_1192,h_670,q_70,strp/when_angels_play_by_dawgit_dji89sc-pre.jpg?" +
-            "token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXN" +
-            "zIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvY" +
-            "2M2NGVmNjQtM2QwNC00YjMyLWI2NTAtZWM4YWFjOGM2ZDViXC9kamk4OXNjLWNiNjg2MzY2LWFjM2ItNDFjYi1hNmNiLTdjMTczMjUxMGUxNC5wbmc" +
-            "iLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.4_ePocHkNMWbBdwlAWNcdH1uaB1ZfakE" +
-            "SJy13EJ6EHY");
-
-
-        imageUrls.Add("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/cc64ef64-3d04-4b32-b650-ec8aac8c6d5b/dbur6i6-7" +
-            "20155fc-14e9-4584-9ba1-90956760d323.png/v1/fit/w_828,h_1472,q_70,strp/apo7x_16550228_2ss_by_dawgit_dbur6i6-414w-2x" +
-            ".jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIi" +
-            "wiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTgyMSIsInBhdGgiOiJc" +
-            "L2ZcL2NjNjRlZjY0LTNkMDQtNGIzMi1iNjUwLWVjOGFhYzhjNmQ1YlwvZGJ1cjZpNi03MjAxNTVmYy0xNGU5LTQ1ODQtOWJhMS05MDk1Njc2MGQzMj" +
-            "MucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.3KGefJaMs3oR6fP4-wrk2Purq" +
-            "s4rhH4gmHnQfQ5DEBc");
-
-
-
-        imageUrls.Add("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/20566ef6-fc61-452b-a5f2-326f8442dbd3/djoukg5-0" +
-            "634e660-05c9-45b1-ace0-e4aba82af9a1.jpg/v1/fill/w_975,h_819,q_70,strp/uf_chain_pong_2354___love_me_a_julia_by_west" +
-            "oz64_djoukg5-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZD" +
-            "QxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA3" +
-            "NSIsInBhdGgiOiJcL2ZcLzIwNTY2ZWY2LWZjNjEtNDUyYi1hNWYyLTMyNmY4NDQyZGJkM1wvZGpvdWtnNS0wNjM0ZTY2MC0wNWM5LTQ1YjEtYWNlMC" +
-            "1lNGFiYTgyYWY5YTEuanBnIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.Xoj3jhAAL" +
-            "7t8PeoOakR0BddT_tAaS-LlJD_prrzAC4g");
+        QueryThreadManager threadManager = new QueryThreadManager();
+        List<string> imageUrls = await threadManager.GetResults();
 
 
         //Recycler view creation
---
+++ ArtViewer/Network.DeviantArt/QueryThreadManager.cs
@@ -0,0 +1,193 @@
+﻿using Android.Drm;
+using System.Collections.Concurrent;
+using System.Text.Json;
+
+namespace ArtViewer.Network.Deviantart
+{
+
+    /// <summary>
+    /// Class that starts threads to connect to the DeviantArt API and retrieve the image URLs.
+    /// </summary>
+    internal class QueryThreadManager
+    {
+        //fetch only 250 imagaes, but it not necessarily the first 250 in the gallery
+        public const int MAX_IMAGES = 250;
+        public const int MAX_QUERY_LIMIT = 24;
+
+
+        private readonly ConcurrentBag<string> urls = new ConcurrentBag<string>();
+        private readonly TaskCompletionSource queriesCompleted = new TaskCompletionSource();
+
+
+
+        
+        public QueryThreadManager()
+        {
+
+            List<string> queries = PlanQueries();
+            StartAllQueries(queries);
+        }
+
+
+
+        /// <summary>
+        /// Generates a list of queries that will fetch the all images from the API, from beginning
+        /// to end (or the MAX_IMAGES limit is reached).
+        /// </summary>
+        private List<string> PlanQueries()
+        {
+            //TODO: this is using sample data. Needs to be replaced with real data
+
+            List<string> queries = new List<string>();
+            int offset = 0;
+            while (offset < MAX_IMAGES)
+            {
+                int queryLimit = MAX_QUERY_LIMIT;
+                string url = NetworkUtils.BuildGenericFolderUrl("gallery", "89DB8DF6-9027-4CD2-965F-27CE55CCEFA9",
+                                                                "dissunder", MAX_QUERY_LIMIT, offset);
+
+                queries.Add(url);
+                offset += MAX_QUERY_LIMIT;
+            }
+
+            return queries;
+        }
+
+
+
+        private async Task StartAllQueries(List<string> urls)
+        {
+
+            await Parallel.ForEachAsync(urls, async (url, ct) =>
+            {
+                try
+                {
+                    await GetImageUrls(url);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Failed to retrieve images: " + e.Message);
+                }
+            });
+
+
+            this.queriesCompleted.SetResult();
+        }
+
+
+
+        /// <summary>
+        /// Runs a query to get images from the DeviantArt API. Since the API limits the amount of data
+        /// that can be retrieved in a single call, we have to make multiple calls to this function, each
+        /// with an offset determaning which images this call should retrieve.
+        /// </summary>
+        private async Task GetImageUrls(string url)
+        {
+            using JsonDocument folder = await NetworkUtils.RunGetRequest(url);
+            JsonElement root = folder.RootElement;
+
+
+            //This will throw an exception if something went wrong with the API call
+            CheckQueryResponseForErrors(root);
+
+
+            SaveQueryResults(root);
+        }
+
+
+
+        /// <summary>
+        /// Checks if something went wrong with the API call. If so, throw an exception.
+        /// </summary>
+        /// <param name="root">Root element of the JSON response</param>
+        private void CheckQueryResponseForErrors(JsonElement root)
+        {
+            bool hasError = root.TryGetProperty("error", out JsonElement error);
+            if (hasError)
+            {
+                Console.WriteLine("Connection Failure: " + error.ToString());
+                throw new HttpRequestException("Connection Failure: " + error.ToString());
+            }
+
+            //Sometimes the API will use the key status instead of error
+            bool hasStatus = root.TryGetProperty("status", out JsonElement status);
+            if (hasStatus && status.ToString() == "error")
+            {
+                var errorMsg = root.GetProperty("message");
+                Console.WriteLine("Connection Failure: " + errorMsg.ToString());
+                throw new HttpRequestException("Connection Failure: " + errorMsg.ToString());
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// Extracts the image urls from the returned data and saves them to the urls list.
+        /// </summary>
+
+        /// <param name="root">the root element of the API response</param>
+        private void SaveQueryResults(JsonElement root)
+        {
+            var pics = root.GetProperty("results");
+            int imagesRecieved = pics.GetArrayLength();
+            int imageCount = pics.GetArrayLength();
+
+
+            for (int i = 0; i < imageCount; i++)
+            {
+                ExtractImage(i);
+            }
+
+
+
+            void ExtractImage(int imageIndex)
+            {
+                var imageData = pics[imageIndex];
+                if (imageData.TryGetProperty("content", out JsonElement content))
+                {
+                    if (content.TryGetProperty("src", out JsonElement src))
+                    {
+                        urls.Add(src.ToString());
+                    }
+                }
+            }
+        }
+
+
+
+        /// <summary>
+        /// Compiles the results from the threads into a single list of random order.
+        /// 
+        /// Note: this function will block until all the data is loaded.
+        /// </summary>
+        public async Task<List<string>> GetResults()
+        {
+            await queriesCompleted.Task;
+
+            List<string> results = new List<string>(this.urls);
+            ShuffleList<string>(results);
+            return results;
+        }
+
+
+
+        /// <summary>
+        /// Randomize the order of the list so the images are displayed in a different order each time.
+        /// </summary>
+        private void ShuffleList<T>(List<T> list)
+        {
+            Random rng = new Random();
+
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
