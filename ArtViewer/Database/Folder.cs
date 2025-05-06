8bcbb38bac8d6e496ac9e72b056d6552fb7bd1a2
avrohomthousman-p <avrohomthousman@gmail.com>
2025-05-06 15:11:08 -0400

    feat: added DB dependency and a model class

---
+++ ArtViewer/Database/Folder.cs
@@ -0,0 +1,52 @@
+﻿using SQLite;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ArtViewer.Database
+{
+    /// <summary>
+    /// Model class for storing information about a gallery/collection that the user has saved and
+    /// images should be loaded from.
+    /// </summary>
+    public class Folder
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; private set; }
+
+
+        [SQLite.MaxLength(255)]
+        [NotNull]
+        public string FolderId { get; set; }
+
+
+        [NotNull]
+        public int TotalImages { get; set; } = Network.Deviantart.QueryThreadManager.MAX_IMAGES;
+
+
+        [SQLite.MaxLength(20)]
+        [NotNull]
+        public string CollectionType { get; set; }
+
+
+        [SQLite.MaxLength(50)]
+        [NotNull]
+        public string Username { get; set; }
+
+
+        [NotNull]
+        public bool ShouldRandomize { get; set; } = true;
+
+
+        public Folder() { }
+
+
+        public Folder(string folderId, int totalImages, string collectionType, string username, bool shouldRandomize)
+        {
+            FolderId = folderId;
+            TotalImages = totalImages;
+            CollectionType = collectionType;
+            Username = username;
+            ShouldRandomize = shouldRandomize;
+        }
+    }
+}
