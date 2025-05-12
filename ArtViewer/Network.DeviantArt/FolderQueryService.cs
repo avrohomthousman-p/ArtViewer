21a43948127e0161590761dd1c1242275cd1e4f5
avrohomthousman-p <avrohomthousman@gmail.com>
2025-05-12 14:55:25 -0400

    refactor: modify folder query service to better support letting user pick folders from list

--- ArtViewer/Network.DeviantArt/FolderQueryService.cs
+++ ArtViewer/Network.DeviantArt/FolderQueryService.cs
@@ -16,38 +16,55 @@ namespace ArtViewer.Network.DeviantArt
     {
 
         /// <summary>
-        /// Fetches the desired folder from the DeviantArt API and saves it to the database for future use.
+        /// Fetches the desired gallery/collection from the DeviantArt API and saves it to the database for future use.
         /// </summary>
-        /// <exception cref="FolderNotFoundException">If the folder could not be found</exception>
-        public async Task SaveFolder(StorageLocation location, string username, string actualFolderName, bool shouldRandomize, bool allPics=false)
+        public async Task SaveFullGalleryOrCollection(StorageLocation location, string username, bool shouldRandomize)
+        {
+            JsonElement foldersArray = await FetchAllUsersFolders(username, location);
+
+
+            Folder folder = CreateFolderForAllImages(foldersArray);
+            folder.StoredIn = location;
+            folder.Username = username;
+            folder.ShouldRandomize = shouldRandomize;
+            folder.CustomName = GetDefaultFolderLabel(location, username, null, true);
+
+
+            StandardDBQueries.CreateFolder(folder);
+        }
+
+
+
+        /// <summary>
+        /// Queries the API for all folders owned by the specified user and returns them as an array
+        /// of UNSAVED folder instances.
+        /// </summary>
+        /// <param name="location">The location of the folders, gallery or collection</param>
+        /// <param name="username">The username of the DeviantArt user who owns the folder</param>
+        /// <returns>An array of unsaved folder instances containing each folder the user has</returns>
+        public async Task GetAllFoldersOwnedByUser(StorageLocation location, string username)
+        {
+            JsonElement foldersArray = await FetchAllUsersFolders(username, location);
+
+            //TODO: convert the JSON data to an array of folders and return it
+        }
+
+
+
+        /// <summary>
+        /// Queries the API and gets all the folders owned by the specified DeviantArt user.
+        /// </summary>
+        /// <param name="username">The name of the DeviantArt user whose folders you want to see</param>
+        /// <param name="location">The location those folders can be found in, Gallery or Collection</param>
+        private async Task<JsonElement> FetchAllUsersFolders(string username, StorageLocation location)
         {
             string url = BuildUrl(location, username);
-            using JsonDocument response = await NetworkUtils.RunGetRequest(url);
+            JsonDocument response = await NetworkUtils.RunGetRequest(url);
             JsonElement root = response.RootElement;
 
 
             CheckResponseForErrors(root);
-            JsonElement foldersArray = root.GetProperty("results");
-
-
-            Folder folder;
-            if (allPics)
-            {
-                folder = CreateFolderForAllImages(foldersArray);
-            }
-            else
-            {
-                folder = FindFolderInResults(foldersArray, actualFolderName, username);
-            }
-
-
-            folder.StoredIn = location;
-            folder.Username = username;
-            folder.ShouldRandomize = shouldRandomize;
-            folder.CustomName = GetDefaultFolderLabel(location, username, actualFolderName, allPics);
-
-
-            StandardDBQueries.CreateFolder(folder);
+            return root.GetProperty("results");
         }
 
 
@@ -82,83 +99,12 @@ namespace ArtViewer.Network.DeviantArt
 
 
         /// <summary>
-        /// Finds the folder in the results array with the correct name
-        /// </summary>
-        private Folder FindFolderInResults(JsonElement foldersArray, string folderName, string username)
-        {
-            JsonElement currentItem;
-            int folderCount = foldersArray.GetArrayLength();
-
-
-            CheckIfUserExists(foldersArray, username);
-
-
-            for (int i = 0; i < folderCount; i++)
-            {
-                currentItem = foldersArray[i];
-                if (FoldersMatch(currentItem, folderName))
-                {
-                    Folder output = new Folder();
-                    output.FolderId = currentItem.GetProperty("folderid").ToString();
-                    output.TotalImages = currentItem.GetProperty("size").GetInt32();
-                    return output;
-                }
-            }
-
-
-            throw new FolderNotFoundException($"Could not find folder named {folderName}");
-        }
-
-
-
-
-        /// <summary>
-        /// If you query the API for a username that does not exist, it returns a single default
-        /// folder that is empty and named featured. So if that is the response we got, we need
-        /// to throw a UserNotFoundException.
-        /// </summary>
-        /// <exception cref="UserNotFoundException"></exception>
-        private void CheckIfUserExists(JsonElement foldersArray, string username)
-        {
-            if(foldersArray.GetArrayLength() != 1)
-            {
-                return;
-            }
-
-            if (foldersArray[0].GetProperty("name").GetString() != "Featured")
-            {
-                return;
-            }
-
-            if(foldersArray[0].GetProperty("size").GetInt32() != 0)
-            {
-                return;
-            }
-
-
-            throw new UserNotFoundException($"Could not find any user named {username}");
-        }
-
-
-
-        /// <summary>
-        /// Checks if the JSON folder data matches the folder name we are looking for.
-        /// </summary>
-        private static bool FoldersMatch(JsonElement folderFound, string desiredFolderName)
-        {
-            return folderFound.GetProperty("name").ToString().ToLower() == desiredFolderName.ToLower();
-        }
-
-
-
-
-        /// <summary>
         /// Creates a folder for all the images in this users collection/gallery
         /// </summary>
         private Folder CreateFolderForAllImages(JsonElement foldersArray)
         {
             int imageCount = 0;
-            for(int i = 0; i < foldersArray.GetArrayLength(); i++)
+            for (int i = 0; i < foldersArray.GetArrayLength(); i++)
             {
                 imageCount += foldersArray[i].GetProperty("size").GetInt32();
             }
@@ -168,6 +114,7 @@ namespace ArtViewer.Network.DeviantArt
             folder.TotalImages = imageCount;
             return folder;
         }
+
 
 
 
@@ -186,33 +133,5 @@ namespace ArtViewer.Network.DeviantArt
                 return actualFolderName;
             }
         }
-    }
-
-
-
-    /// <summary>
-    /// Thrown when the DeviantArt folder cannot be found
-    /// </summary>
-    internal class FolderNotFoundException : Exception
-    {
-        public FolderNotFoundException() { }
-
-        public FolderNotFoundException(string message) : base(message) { }
-
-        public FolderNotFoundException(string message, Exception inner) : base(message, inner) { }
-    }
-
-
-
-    /// <summary>
-    /// Thrown when the DeviantArt user does not exist
-    /// </summary>
-    internal class UserNotFoundException : Exception
-    {
-        public UserNotFoundException() { }
-
-        public UserNotFoundException(string message) : base(message) { }
-
-        public UserNotFoundException(string message, Exception inner) : base(message, inner) { }
     }
 }
