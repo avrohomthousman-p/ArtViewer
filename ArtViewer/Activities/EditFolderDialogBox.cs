067bb7478ea2d9dbb3b6a748e65b4fa539982344
avrohomthousman-p <avrohomthousman@gmail.com>
2025-05-12 19:38:31 -0400

    feat: made base class for building dialog box

--- ArtViewer/Activities/EditFolderDialogBox.cs
+++ ArtViewer/Activities/EditFolderDialogBox.cs
@@ -9,91 +9,27 @@ namespace ArtViewer.Activities
     /// <summary>
     /// Custom AlertDialog popup designed to edit user folders
     /// </summary>
-    public class EditFolderDialogBox
+    public class EditFolderDialogBox : GenericDialogBox
     {
-        protected readonly AndroidX.AppCompat.App.AlertDialog.Builder builder;
-        protected readonly Activity activity;
-        protected readonly Folder folder;
-
-        protected EditText popupInputField;
         protected TextView originalFolderNameTextView;
 
 
-        public string Title { get; set; } = "Edit Folder Label";
+        public override string Title { get; set; } = "Edit Folder Label";
 
 
 
-        public EditFolderDialogBox(Activity activity, TextView originalFolderNameTextView, Folder folder)
+        public EditFolderDialogBox(Activity activity, Folder folder, TextView originalFolderNameTextView) : base(activity, folder)
         {
-            this.activity = activity;
             this.originalFolderNameTextView = originalFolderNameTextView;
-            this.folder = folder;
-
-            this.builder = new AndroidX.AppCompat.App.AlertDialog.Builder(activity);
-            this.builder.SetTitle(this.Title);
-            this.builder.SetView(BuildMainContents());
-            this.builder.SetNegativeButton("Cancel", (sender, e) => { });
-
-            //Use null to avoid the default event handler (which closes on click)
-            builder.SetPositiveButton("Save", (IDialogInterfaceOnClickListener)null);
         }
 
 
 
-        /// <summary>
-        /// Builds the main view to be displayed in the dialog.
-        /// </summary>
-        protected View BuildMainContents()
+        protected override void SetupEventHandlers(AndroidX.AppCompat.App.AlertDialog dialogBox)
         {
-            this.popupInputField = new EditText(this.activity)
-            {
-                Text = this.folder.CustomName
-            };
+            base.SetupEventHandlers(dialogBox);
 
-            this.popupInputField.SetBackgroundResource(Resource.Drawable.rounded_edit_text);
-            this.popupInputField.SetPadding(15, 20, 0, 20);
-            this.popupInputField.SetSingleLine(true);
-
-
-            //Setup Margins
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.MatchParent,
-                ViewGroup.LayoutParams.WrapContent
-            );
-
-            layoutParams.SetMargins(20, 0, 20, 0);
-            this.popupInputField.LayoutParameters = layoutParams;
-
-
-            //For the margins to work, the input needs to be inside a layout
-            LinearLayout container = new LinearLayout(this.activity)
-            {
-                Orientation = Orientation.Horizontal
-            };
-
-            container.AddView(this.popupInputField);
-
-            return container;
-        }
-
-
-
-        /// <summary>
-        /// Displays the dialog box
-        /// </summary>
-        /// <returns>A reference to the instance of the running dialog box</returns>
-        public AndroidX.AppCompat.App.AlertDialog ShowDialogBox()
-        {
-            AndroidX.AppCompat.App.AlertDialog dialogBox = this.builder.Create();
-            dialogBox.Show();
-
-
-            //Provide a custom click handler that only closes if the data is valid (can only be done while the dialog is visible)
-            Button saveBtn = dialogBox.GetButton((int)DialogButtonType.Positive);
-            saveBtn.Click += (sender, e) => { SaveButton_HandleClick(dialogBox); };
-
-
-            return dialogBox;
+            this.saveBtn.Click += (sender, e) => { SaveButton_HandleClick(dialogBox); };
         }
 
 
@@ -107,7 +43,7 @@ namespace ArtViewer.Activities
         {
             View rootView = this.activity.FindViewById(Android.Resource.Id.Content);
             string originalName = this.originalFolderNameTextView.Text;
-            string newName = this.popupInputField.Text;
+            string newName = this.folderLabelInputField.Text;
 
 
 
