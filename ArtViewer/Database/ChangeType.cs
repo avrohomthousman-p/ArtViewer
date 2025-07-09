d6926ff97dc25cd2a29fdfca27b2da07a3e2b664
avrohomthousman-p <avrohomthousman@gmail.com>
2025-07-08 22:41:55 -0400

    feat: set up backend for refresh code

---
+++ ArtViewer/Database/ChangeType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtViewer.Database
+{
+    /// <summary>
+    /// Class for tracking different kinds of changes that were done or need to be done
+    /// to a folder.
+    /// </summary>
+    public enum ChangeType
+    {
+        NO_CHANGE, UPDATE, DELETE
+    }
+}
