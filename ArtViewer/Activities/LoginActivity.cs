2a36c12549de8f1efaac76a135772573f8c9df8b
avrohomthousman-p <avrohomthousman@gmail.com>
2025-07-09 21:44:08 -0400

    feat: build login page

---
+++ ArtViewer/Activities/LoginActivity.cs
@@ -0,0 +1,53 @@
+using Android.Content;
+using AndroidX.AppCompat.App;
+using ArtViewer.Network.DeviantArt;
+using Java.Util.Prefs;
+
+namespace ArtViewer.Activities;
+
+
+
+/// <summary>
+/// Startup activity that redirects the user to a browser to log in to DeviantArt.
+/// DeviantArt will redirect the user back to the App and provide us with an access
+/// token.
+/// </summary>
+[Activity(MainLauncher = true, Label = "Login Page")]
+public class LoginActivity : AppCompatActivity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        SetContentView(Resource.Layout.activity_login);
+
+        AndroidX.AppCompat.Widget.Toolbar toolbar = FindViewById<AndroidX.AppCompat.Widget.Toolbar>(Resource.Id.toolbar);
+        SetSupportActionBar(toolbar);
+
+
+        //TODO: add option for logging in with personal token
+
+        Button loginBtn = FindViewById<Button>(Resource.Id.login_btn);
+        loginBtn.Click += (sender, args) => LaunchWebLogin();
+    }
+
+
+
+    /// <summary>
+    /// Luanches the browser so the user can login.
+    /// </summary>
+    private void LaunchWebLogin()
+    {
+        string authUrl = "https://www.deviantart.com/oauth2/authorize" +
+                 "?response_type=token" +
+                 "&client_id=" + Secrets.client_id +
+                 "&redirect_uri=artviewer://oauth2redirect" +
+                 "&scope=browse user.profile" +
+                 "&state=some_random_state" + 
+
+
+        var uri = Android.Net.Uri.Parse(authUrl);
+        var intent = new Intent(Intent.ActionView, uri);
+        StartActivity(intent);
+    }
+}
