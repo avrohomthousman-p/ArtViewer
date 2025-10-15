88d945fae5daaf2f89d319e750027223e5d77dcc
avrohomthousman-p <avrohomthousman@gmail.com>
2025-10-15 07:50:51 -0400

    feat: store all data not just urls

---
+++ ArtViewer/Network.DeviantArt/MediaItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtViewer.Network.DeviantArt
+{
+    /// <summary>
+    /// Data class that stores imformation about an image/video that will be needed to display it.
+    /// </summary>
+    class MediaItem
+    {
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public int Index { get; set; } = -1;
+        public bool IsImage { get; set; } = true;
+
+
+        public MediaItem(string url, string title, bool isImage = true, int index = -1)
+        {
+            this.Url = url;
+            this.Title = title;
+            this.IsImage = isImage;
+            this.Index = index;
+        }
+    }
+}
